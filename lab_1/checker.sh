f410d3de43b27faecd6cb51991d5c7875f103335
rudast <ruslan5haertdinov@yandex.ru>
2025-09-21 21:25:26 +0300

    add checker.sh

--- lab_1/checker.sh
+++ lab_1/checker.sh
@@ -1,23 +1,62 @@
 #!/bin/bash
 
-currentPath=$1
-X=${2:-70}
-N=${3:-5}
+# necessary parametrs
+logDisk=$1
+backupDisk=$2
 
-if [[ $currentPath == "" ]]; then
-    echo "Не указан путь до директории"
-    exit
+if [[ ! -d "$logDisk" ]]; then
+    echo "[ERROR]: Set true path to the disk with /log as first parametr."
+    exit 1
 fi
+
+if [[ ! -d "$backupDisk" ]]; then
+    echo "[ERROR]: Set true path to the disk with /backup as second parametr."
+fi
+
+# other parametrs
+X=${3:-70}
+N=${4:-5}
 
 if [[ $X -lt 0 || $X -gt 100 ]]; then 
-    echo "Укажите корректный X%"
+    echo "[ERROR]: Set true parcentage X as third parametr."
     exit
 fi
 
-filesCount=$(echo find $currentPath -type f | wc -l)
+if [[ $N -lt 0 ]]; then 
+    echo "[ERROR]: Set true count of compressiong files N as 4th parametr."
+    exit
+fi
 
+diskUsage=$(df "$logDisk" | awk 'NR==2 {gsub("%",""); print $5}')
+filesCount=$(find $logDisk -type f | wc -l)
 
-echo $currentPath
-echo $X
-echo $N
-echo $filesCount
+echo "[INFO]: diskUsage = ${diskUsage}, filesCount = ${filesCount}, X = ${X}, N = ${N}"
+
+if [[ $diskUsage -lt $X ]]; then
+    echo "Compression is not demanded. Complete!"
+elif [[ $filesCount -ge $N ]]; then
+    mapfile -t files < <(ls -t "$logDisk" | head -n "$N")
+    
+    time=$(date +%Y-%m-%d_%H-%M-%S)
+    archiveName="backup_$time.tar.gz"
+    echo -n "[INFO]: Files: "
+    for file in "${files[@]}"; do
+        echo -n "$file "
+    done
+    echo "."
+
+    echo -n "Files are compressing..."
+
+    tar -czf "${backupDisk}/${archiveName}" -C "${logDisk}" "${files[@]}"
+
+    echo -n "Old files are deleting..."
+
+    for file in "${files[@]}"; do
+        rm -f "${logDisk}/${file}"
+    done
+
+    echo -e "\rFiles were deleted and compressed in ${backupDisk}/${archiveName}. Complete!"
+    
+else 
+    echo "There are too few files in the directory. Need $N files for compression."
+fi
